117e1d97bc2bbc1e369be818e2ef6bed51365812
aytronn <titouan.desp@orange.fr>
2022-01-02 02:11:02 +0100

    First version v0.1

--- src/main/java/fr/aytronn/storyhard/StoryHard.java
+++ src/main/java/fr/aytronn/storyhard/StoryHard.java
@@ -4,6 +4,7 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import fr.aytronn.storyhard.config.Configuration;
 import fr.aytronn.storyhard.config.Persist;
+import fr.aytronn.storyhard.listeners.PlayerListener;
 import fr.aytronn.storyhard.tools.threads.CustomThread;
 import net.md_5.bungee.api.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -16,6 +17,11 @@ public final class StoryHard extends JavaPlugin {
     private Persist persist;
     private Configuration configuration;
 
+
+    /**
+     * On plugin is enabled
+     *
+     */
     @Override
     public void onEnable() {
         try {
@@ -33,23 +39,62 @@ public final class StoryHard extends JavaPlugin {
 
             getLogger().info(ChatColor.YELLOW
                     + "Configuration loaded (" + (System.currentTimeMillis() - startMillis) + ") ms.");
+            startMillis = System.currentTimeMillis();
+
+            getLogger().info(ChatColor.YELLOW + "Loading class");
+            registerListeners();
+            registerCommands();
+            getLogger().info(ChatColor.YELLOW
+                    + "Class loaded (" + (System.currentTimeMillis() - startMillis) + ") ms.");
         } catch (Exception e) {
             getLogger().severe(e.getMessage());
             e.printStackTrace();
         }
     }
 
-
-
+    /**
+     * On plugin disabled
+     *
+     */
     @Override
     public void onDisable() {
+        long startMillis = System.currentTimeMillis();
+        getLogger().info(ChatColor.BLUE + "==========- " + ChatColor.GOLD
+                + "StoryHard shutdown" + ChatColor.BLUE + " -==========");
+        getLogger().info(ChatColor.YELLOW + "Shutdown thread");
         CustomThread.shutdownAll();
+        getLogger().info(ChatColor.YELLOW
+                + "Thread shutdown (" + (System.currentTimeMillis() - startMillis) + ") ms.");
     }
 
+    /**
+     * Allow to get instance of this plugin
+     *
+     * @return instance of plugin
+     */
     public static StoryHard getInstance() {
         return instance;
     }
 
+    /**
+     * Allow registering commands
+     *
+     */
+    private void registerCommands() {
+    }
+
+    /**
+     * Allow registering listeners
+     *
+     */
+    private void registerListeners() {
+        getInstance().getServer().getPluginManager().registerEvents(new PlayerListener(), this);
+    }
+
+    /**
+     * Allow loading configuration
+     *
+     */
     private void loadConfiguration() {
         if (getPersist().getFile(Configuration.class).exists()) {
             configuration = getPersist().load(Configuration.class);
@@ -59,14 +104,29 @@ public final class StoryHard extends JavaPlugin {
         }
     }
 
+    /**
+     * Allow to get the configuration
+     *
+     * @return The configuration
+     */
     public Configuration getConfiguration() {
         return configuration;
     }
 
+    /**
+     * Allow to get the gson serializer
+     *
+     * @return Gson serializer
+     */
     public Gson getGson() {
         return gson;
     }
 
+    /**
+     * Allow getting persist class
+     *
+     * @return Persist class
+     */
     public Persist getPersist() {
         return persist;
     }
